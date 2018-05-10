eb846d7a23e30a17b2094015a4d1177f6dadb4da
yassine <em_mohamadi@esi.dz>
2018-05-10 14:20:23 -0700

    adding the function that read the data set iris

--- PPV.py
+++ PPV.py
@@ -1,46 +1,21 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn import datasets
-from sklearn.decomposition import PCA
+import csv
+import random
 
-# import some data to play with
-iris = datasets.load_iris()
-print(iris.size)
-'''
-X = iris.data[:, :2]  # we only take the first two features.
-y = iris.target
+#une fonction qui li les données ou bien le data set Iris 
+def loadDataset(filename, split, trainingSet=[] , testSet=[]):
+	with open(filename, 'rt') as csvfile:
+	    lines = csv.reader(csvfile)
+	    dataset = list(lines)
+	    for x in range(len(dataset)):
+	        for y in range(4):
+	            dataset[x][y] = float(dataset[x][y])
+	        if random.random() < split:
+	            trainingSet.append(dataset[x])
+	        else:
+	            testSet.append(dataset[x])
 
-x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-
-plt.figure(2, figsize=(8, 6))
-plt.clf()
-
-# Plot the training points
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1,
-            edgecolor='k')
-plt.xlabel('Sepal length')
-plt.ylabel('Sepal width')
-
-plt.xlim(x_min, x_max)
-plt.ylim(y_min, y_max)
-plt.xticks(())
-plt.yticks(())
-
-# To getter a better understanding of interaction of the dimensions
-# plot the first three PCA dimensions
-fig = plt.figure(1, figsize=(8, 6))
-ax = Axes3D(fig, elev=-150, azim=110)
-X_reduced = PCA(n_components=3).fit_transform(iris.data)
-ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=y,
-           cmap=plt.cm.Set1, edgecolor='k', s=40)
-ax.set_title("First three PCA directions")
-ax.set_xlabel("1st eigenvector")
-ax.w_xaxis.set_ticklabels([])
-ax.set_ylabel("2nd eigenvector")
-ax.w_yaxis.set_ticklabels([])
-ax.set_zlabel("3rd eigenvector")
-ax.w_zaxis.set_ticklabels([])
-
-plt.show()
-'''
+'''trainingSet=[]
+testSet=[]
+loadDataset('iris.data', 0.66, trainingSet, testSet)
+print('Train: ' + repr(len(trainingSet)))
+print('Test: ' + repr(len(testSet)))'''
